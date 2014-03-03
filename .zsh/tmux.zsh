f771d483f03179f0ff6d5c2c67ba2f5172b213a3
Parker Coates <coates@qps.nl>
2014-03-03 08:56:33 -0400

    Make tmux use TERM=screen-256color if 256 colors are actually available.
    The general idea is stolen from oh-my-zsh, but my implementation is
    less fancy.

--- .zsh/tmux.zsh
+++ .zsh/tmux.zsh
@@ -1,16 +1,27 @@
+function term-aware-tmux()
+{
+    local conf=/tmp/tmux.$RANDOM.conf
+    echo "source ~/.tmux.conf" > $conf
+    [[ $(tput colors) -ge 256  ]] && echo "set-option -g default-terminal screen-256color" >> $conf
+    tmux -f $conf $@
+    rm $conf
+}
+
+alias tmux=term-aware-tmux
+
 function t()
 {
     if [[ -z "$TMUX" ]]; then
-        tmux start-server
-        if tmux has-session -t $USER; then
-            tmux attach-session -t $USER
+        \tmux start-server
+        if \tmux has-session -t $USER; then
+            \tmux attach-session -t $USER
         else
-            tmux new-session -s $USER "$argv[1,-1]"
+            term-aware-tmux new-session -s $USER "$argv[1,-1]"
         fi
     elif [[ "$argv[1]" = "-h" || "$argv[1]" = "-v" ]]; then
-        tmux split-window $argv[1] "$argv[2,-1]"
+        \tmux split-window $argv[1] "$argv[2,-1]"
     else
-        tmux new-window "$argv[1,-1]"
+        \tmux new-window "$argv[1,-1]"
     fi
 }
 
