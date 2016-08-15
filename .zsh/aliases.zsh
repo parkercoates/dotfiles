a5f327d1b2e7a144e6c9273159e4349b043da51e
Parker Coates <coates@qps.nl>
2016-08-15 13:14:04 -0300

    Switch back to --date-order when running Gitk.
    While less readable in general, it's just to useful when doing merge
    forensics.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,7 +10,7 @@ alias grep='grep --color=auto --perl-regexp'
 
 # Development conveniences
 alias sshudo='eval `ssh-agent`; ssh-add; '
-alias gk='gitk --all &>/dev/null&|'
+alias gk='gitk --all --date-order &>/dev/null&|'
 alias gc='git cola &>/dev/null&|'
 alias kd='kdevelop &>/dev/null&|'
 alias as='assistant &>/dev/null&|'
