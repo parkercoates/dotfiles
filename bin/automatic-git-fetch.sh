011594bf8ca94018a0f8b3fea07c54b2e8db06ba
Parker Coates <coates@qps.nl>
2017-03-31 09:55:19 -0300

    Update automatic-git-fetch for the new unified repo
    Also fixed an issue trying to pipe stderr.

--- bin/automatic-git-fetch.sh
+++ bin/automatic-git-fetch.sh
@@ -2,10 +2,10 @@
 
 logFile=/home/coates/automatic-git-fetch.log
 
-for repo in /home/coates/source/fm-src.bare /home/coates/source/libraries-shared.bare; do
+for repo in /home/coates/source/qps-dev.git; do
     echo >> $logFile
     date --iso-8601=seconds >> $logFile
     echo $repo >> $logFile
-    cd $repo && git fetch --prune --all 2>&1 >> $logFile 
+    cd $repo && git fetch --prune --all >> $logFile 2>&1
 done
 
